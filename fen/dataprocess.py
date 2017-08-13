524d5b21a000bda0e54a3715d0c251d4fb042c22
FryeT <fengleitan@hotmail.com>
2017-08-13 21:14:24 +0800

    processed datas

---
+++ fen/dataprocess.py
@@ -0,0 +1,16 @@
+
+import pandas as pd
+import os
+
+dirpath = 'datas'
+
+files = os.listdir(dirpath)
+files.sort(key=lambda x: int(os.path.splitext(x)[0]))
+dfcols = ['s_200','s_400','m_200','m_400','s_c','m_c']
+df = pd.DataFrame(columns=dfcols)
+for file in files:
+    temp_df = pd.read_table(os.sep.join((dirpath,file)))
+    temp_df.columns = dfcols
+    df = pd.concat([df,temp_df],ignore_index=True)
+
+df.to_csv('processed_data.csv')
